65e3fa20133f7f4fda477bb7e11d33c9beb460d9
Gautam Kumar Singh <gautam.singh@konicaminolta.com>
2021-06-01 20:17:16 +0800

    added addUSer feature

--- TrackingApp/TrackingApp/Controllers/CreateUserVC.swift
+++ TrackingApp/TrackingApp/Controllers/CreateUserVC.swift
@@ -9,15 +9,93 @@ import UIKit
 
 class CreateUserVC: UIViewController {
 
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtUserName: UITextField!
+    @IBOutlet weak var txtUser: UITextField!
+    
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPhone: UITextField!
+    @IBOutlet weak var txtStreet: UITextField!
+    
+    
+    @IBOutlet weak var txtSuite: UITextField!
+    @IBOutlet weak var txtCity: UITextField!
+    @IBOutlet weak var txtZipcode: UITextField!
+    
+    @IBOutlet weak var btnSearch: RoundedButton!
+    @IBOutlet weak var lblLattitude: UILabel!
+    @IBOutlet weak var lblLongitude: UILabel!
+    
+    @IBOutlet weak var txtCompName: UITextField!
+    @IBOutlet weak var txtCompPhrase: UITextField!
+    @IBOutlet weak var txtCompBs: UITextField!
+    
+    
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var baseScrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.isHidden = false
-     
+        
+        
+        self.txtUser.delegate = self
+        self.txtUserName.delegate = self
+        self.txtPassword.delegate = self
+        
+        self.txtEmail.delegate = self
+        self.txtPhone.delegate = self
+        self.txtStreet.delegate = self
+        
+        self.txtSuite.delegate = self
+        self.txtCity.delegate = self
+        self.txtZipcode.delegate = self
+        
+        self.txtCompName.delegate = self
+        self.txtCompPhrase.delegate = self
+        self.txtCompBs.delegate = self
+        
+        
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(CreateUserVC.keyboardWillShow(_:)), name:UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(CreateUserVC.keyboardWillHide(_:)), name:UIResponder.keyboardWillHideNotification, object: nil)
     }
     @IBAction func didCancelUserTapped(){
         self.dismiss(animated: true, completion: nil)
     }
     @IBAction func didSaveUserTapped(){
         self.dismiss(animated: true, completion: nil)
+    }
+    @objc func keyboardWillShow(_ notification: Notification) {
+        let userInfo: NSDictionary = notification.userInfo! as NSDictionary
+        let keyboardInfo = userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue
+        let keyboardSize = keyboardInfo.cgRectValue.size
+        let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+        self.baseScrollView.contentInset = contentInsets
+        self.baseScrollView.scrollIndicatorInsets = contentInsets
+    }
+    @objc func keyboardWillHide(_ notification: Notification) {
+        self.baseScrollView.contentInset = .zero
+        self.baseScrollView.scrollIndicatorInsets = .zero
+    }
+    @IBAction func didSearchTapped(_ sender: Any) {
+       
+    }
+}
+//MARK:- UITextFieldDelegate
+extension CreateUserVC:UITextFieldDelegate{
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.tag == 1 {
+            self.txtPassword.becomeFirstResponder()
+            return true
+        }else{
+            return textField.resignFirstResponder()
+        }
     }
 }
